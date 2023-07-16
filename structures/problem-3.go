2105ea337ffac54a13a58888158ad77fd5e466c3
Sergio Teixeira <sergio@triggerise.org>
2023-07-16 21:35:10 +0100

    problem3

---
+++ structures/problem-3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	for i <= 100 {
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+
+		} else {
+			fmt.Println(i)
+		}
+		i += 1
+	}
+}
